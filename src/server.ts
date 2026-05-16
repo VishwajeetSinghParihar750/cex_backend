15b62c3cc48b900473af4ebe7f1f37a78cacbf48
Vishwajeet Singh Parihar <svishwajeet750@gmail.com>
2026-05-16 05:06:47 +0000

    Reapply "import"
    This reverts commit a6a796bce43185bd6221909b174c3e26427541a1.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import app from "./app.js";
-import { redisClient as globalRedisClient } from "./db/redis/index.js";
 import { wsServer } from "./ws/index.js";
 import { wsEngineInterface } from "./ws/handlers/index.js";
 
